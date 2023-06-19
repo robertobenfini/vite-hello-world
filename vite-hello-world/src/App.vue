2c2bdaae575fd8a73b227d1ea294d65484369c1a
robertobenfini <roberto.benfini@gmail.com>
2023-06-19 15:12:08 +0200

    first commit

---
+++ vite-hello-world/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+ import AppTitle from './components/AppTitle.vue';
+
+ export default {
+     components:{
+       AppTitle
+   }
+ }
+</script>
+
+<template>
+  <div>
+    <AppTitle />
+  </div>
+</template>
+
+<style scoped>
+  
+</style>
